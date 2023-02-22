4e5a2f7f6437affd67098362eea46e87ce3d69d4
Chris Johnston <cjohnston@connected.io>
2023-02-22 09:02:25 -0500

    select product mostly complete

--- Ruby/spec/vending_machine_spec.rb
+++ Ruby/spec/vending_machine_spec.rb
@@ -1,7 +1,88 @@
 require './src/vending_machine'
 
 RSpec.describe VendingMachine do
-  it 'should work' do
-    expect(true).to eq(true)
+  subject { VendingMachine.new }
+
+  describe 'inserting coins' do
+    it 'should display INSERT COIN when no coins have been inserted' do
+      expect(subject.display).to eq('INSERT COIN')
+    end
+
+    it 'should display 0.25 when a quarter is inserted' do
+      subject.accept_coin(:quarter)
+      expect(subject.display).to eq('0.25')
+    end
+
+    it 'should display 0.10 when a dime is inserted' do
+      subject.accept_coin(:dime)
+      expect(subject.display).to eq('0.10')
+    end
+
+    it 'should display 0.05 when a nickel is inserted' do
+      subject.accept_coin(:nickel)
+      expect(subject.display).to eq('0.05')
+    end
+
+    it 'should display the total when multiple coins are inserted' do
+      subject.accept_coin(:dime)
+      subject.accept_coin(:quarter)
+
+      expect(subject.display).to eq('0.35')
+    end
+
+    it 'should reject pennies' do
+      subject.accept_coin(:penny)
+      expect(subject.display).to eq('INSERT COIN')
+      expect(subject.coin_return).to eq(:penny)
+    end
+  end
+
+  describe 'selecting item' do
+    it 'should return nothing when cola is selected and no money is inserted' do
+      result = subject.select_item(:cola)
+      expect(result).to eq(nil)
+      expect(subject.display).to eq("PRICE 1.00")
+    end
+
+    it 'should return a cola when selected and given enough change' do
+      subject.accept_coin :quarter
+      subject.accept_coin :quarter
+      subject.accept_coin :quarter
+      subject.accept_coin :quarter
+
+      result = subject.select_item :cola
+
+      expect(result).to eq(:cola)
+      expect(subject.display).to eq('THANK YOU')
+    end
+
+    it 'should return chips when selected and total is 0.50' do
+      subject.accept_coin :quarter
+      subject.accept_coin :quarter
+
+      result = subject.select_item :chips
+
+      expect(result).to eq(:chips)
+      expect(subject.display).to eq('THANK YOU')
+    end
+
+    it 'should return candy when selected and total is 0.65' do
+      subject.accept_coin :quarter
+      subject.accept_coin :quarter
+      subject.accept_coin :dime
+      subject.accept_coin :nickel
+
+      result = subject.select_item :candy
+
+      expect(result).to eq(:candy)
+      expect(subject.display).to eq('THANK YOU')
+    end
+
+    it 'should display the price when an item is selected and not enough money is given' do
+      subject.accept_coin :quarter
+      result = subject.select_item :candy
+      expect(result).to eq(nil)
+      expect(subject.display).to eq('PRICE 0.65')
+    end
   end
 end
--- Ruby/src/vending_machine.rb
+++ Ruby/src/vending_machine.rb
@@ -1,3 +1,61 @@
-class VendingMachine
+# frozen_string_literal: true
 
+# describes a product that the vending machine sells
+class Product
+  attr_reader :name, :price
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+end
+
+# Describes a vending machine that accepts coins to return products
+class VendingMachine
+  attr_accessor :total, :display_text
+
+  MONEY = {
+    quarter: 25,
+    dime: 10,
+    nickel: 5
+  }
+
+  PRODUCTS = {
+    cola: Product.new(:cola, 100),
+    chips: Product.new(:chips, 50),
+    candy: Product.new(:candy, 65)
+  }
+
+  def initialize
+    @total = 0
+    @display_text = 'INSERT COIN'
+  end
+
+  def accept_coin(coin)
+    return if coin == :penny
+
+    self.total += MONEY[coin]
+
+    self.display_text = sprintf('%.2f', total / 100.00)
+  end
+
+  def select_item(selection)
+    item = PRODUCTS[selection]
+
+    if total == item.price
+      self.display_text = 'THANK YOU'
+      item.name
+    else
+      self.display_text = "PRICE #{sprintf('%0.2f', item.price / 100.00)}"
+      nil
+    end
+  end
+
+  def display
+    display_text
+  end
+
+  def coin_return
+    :penny
+  end
 end
